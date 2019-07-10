feb0bbee3202d9adf2ecc993146d6572c7a99f97
Tao Siyu <alextao52100313@gmail.com>
2019-07-10 13:32:40 +0800

    7.10
    7.10

---
+++ SpringBoot/src/main/java/com/alextao/springboot/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.alextao.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+	@ResponseBody
+	@RequestMapping(value = "/hello")
+	private String hello() {
+		return "Hello world ~~";
+	}
+
+}
